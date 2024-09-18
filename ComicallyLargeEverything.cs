79217d7ae16b7c913d9548a90282afccbfc39994
alfuwu <130256600+alfuwu@users.noreply.github.com>
2024-09-17 23:07:24 -0700

    1.2 upd

--- ComicallyLargeEverything.cs
+++ ComicallyLargeEverything.cs
@@ -1,18 +1,14 @@
 using System.ComponentModel;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
 
-namespace ComicallyLargeEverything
-{
-    public class ComicallyLargeEverything : Mod
-    {
+namespace ComicallyLargeEverything {
+    public class ComicallyLargeEverything : Mod {
         // nothing to put here
     }
     
-    public class Config : ModConfig
-    {
+    public class Config : ModConfig {
         public override ConfigScope Mode => ConfigScope.ServerSide;
 
         [DefaultValue(true)]
@@ -31,7 +27,7 @@ namespace ComicallyLargeEverything
         [ReloadRequired]
         public float Size = 1;
 
-        [DefaultValue(1)]
+        [DefaultValue(1f)]
         [Range(0.1f, 10f)]
         [ReloadRequired]
         public float UseTime = 1;
@@ -47,31 +43,22 @@ namespace ComicallyLargeEverything
         public float Value = 1;
     }
 
-    public class ComicallyLarge : ModPrefix
-    {
+    public class ComicallyLarge : ModPrefix { // doesn't apply to copper bows(???)
         public override PrefixCategory Category => PrefixCategory.AnyWeapon; // sadly accessories cant be made big (easily) :c
 
-        public override float RollChance(Item item)
-        {
-            return ModContent.GetInstance<Config>().RollChance;
+        public override float RollChance(Item item) => ModContent.GetInstance<Config>().RollChance;
+
+        public override bool CanRoll(Item item) => ModContent.GetInstance<Config>().Enabled;// && item.useStyle != ItemUseStyleID.Thrust; // no short swords (doesn't work)
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus) {
+            useTimeMult *= 2 * ModContent.GetInstance<Config>().UseTime * ModContent.GetInstance<Config>().Size;
+            knockbackMult *= 2 * ModContent.GetInstance<Config>().Size * ModContent.GetInstance<Config>().Knockback;
+            scaleMult *= 10 * ModContent.GetInstance<Config>().Size;
+            damageMult *= 2 * ModContent.GetInstance<Config>().Size;
         }
 
-        public override bool CanRoll(Item item)
-        {
-            return ModContent.GetInstance<Config>().Enabled && item.useStyle != ItemUseStyleID.Thrust; // no short swords (doesn't work)
-        }
-
-        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
-        {
-            useTimeMult *= (float) ((2 * ModContent.GetInstance<Config>().UseTime) * ModContent.GetInstance<Config>().Size);
-            knockbackMult *= (float) (2 * ModContent.GetInstance<Config>().Size * ModContent.GetInstance<Config>().Knockback);
-            scaleMult *= (float) (10 * ModContent.GetInstance<Config>().Size);
-            damageMult *= (float) (2 * ModContent.GetInstance<Config>().Size);
-        }
-
-        public override void ModifyValue(ref float valueMult)
-        {
-            valueMult *= (float) (1.25 * ModContent.GetInstance<Config>().Value);
+        public override void ModifyValue(ref float valueMult) {
+            valueMult *= 1.25f * ModContent.GetInstance<Config>().Value;
         }
     }
 }
---
+++ ComicallyLargeProjectile.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace ComicallyLargeEverything {
+    public class ComicallyLargeProjectile : GlobalProjectile {
+        //public override bool InstancePerEntity => true;
+        //public Item sourceItem;
+
+        public override void OnSpawn(Projectile projectile, IEntitySource source) {
+            if (source is EntitySource_ItemUse itemSource) {
+                if (itemSource.Item.prefix == ModContent.PrefixType<ComicallyLarge>()) {
+                    projectile.scale *= 10 * ModContent.GetInstance<Config>().Size;
+                    if (projectile.aiStyle == ProjAIStyleID.Spear || projectile.aiStyle == ProjAIStyleID.ShortSword) // these items need to get a velocity buff, otherwise they're too slow
+                        projectile.velocity *= 4.5f * ModContent.GetInstance<Config>().Size / ModContent.GetInstance<Config>().UseTime;
+                }
+            }
+        }
+    }
+}
